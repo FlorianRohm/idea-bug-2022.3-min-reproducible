fc8583de55125c5775d754382dcd87eb1e14e3cd
Florian Rohm <rohm.florian@googlemail.com>
2022-11-16 22:45:56 +0100

    minimum reproducible version of bug

---
+++ spa/src/testComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bar"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<style lang="scss">
+@import '@/styles/foo';
+
+.bar {
+  background-color: $c;
+}
+</style>
---
+++ spa/src/testImport.vue
@@ -0,0 +1,14 @@
+<template>
+  <TestComponent></TestComponent>
+</template>
+
+<script setup lang="ts">
+import TestComponent from '@/testComponent.vue';</script>
+
+<style lang="scss">
+@import '@/styles/foo';
+
+.bar {
+  background-color: $c;
+}
+</style>
